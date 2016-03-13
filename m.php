2f7e46eaa5c5b324db52400a36331240f841bcbe
er0k <er0k@er0k.net>
2016-03-13 17:59:40 -0400

    clean up mpd json relay

--- m.php
+++ m.php
@@ -6,20 +6,12 @@ ksort($_REQUEST);
 $args = array_values($_REQUEST);
 
 require_once('MPD.php');
-$mpd = new MPD('deb.r0k');
+$mpd = new MPD();
 
-if (!empty($args)) {
-    try {
-        $result = call_user_func_array(array($mpd, $cmd), $args);
-    } catch (MPDException $e) {
-        $result = array('error' => $e->getMessage());
-    }
-} else {
-    try {
-        $result = call_user_func(array($mpd, $cmd));
-    } catch (MPDException $e) {
-        $result = array('error' => $e->getMessage());
-    }
+try {
+    $result = call_user_func_array(array($mpd, $cmd), $args);
+} catch (MPDException $e) {
+    $result = array('error' => $e->getMessage());
 }
 
 echo json_encode($result);
